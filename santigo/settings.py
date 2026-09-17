8631cf84483de8df7e7d7d08966e662c08258952
vintech <nnajivictor276@gmail.com>
2026-09-17 19:24:59 +0100

    add profile

--- santigo/settings.py
+++ santigo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$5-=58w+35+en-ap7(7at#6w1q(+_$i62w*eh2g*jxltw-5a_7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
